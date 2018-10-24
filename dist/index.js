c61efe1d2829e5b64f24d199d84143443ef65793
Joff Tiquez <jofftiquez@gmail.com>
2018-10-24 13:34:25 +0800

    New build

--- dist/index.js
+++ dist/index.js
@@ -74,51 +74,119 @@ module.exports =
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.NsVueNami = undefined;
+
+var _nami = __webpack_require__(1);
+
+var NsVueNami = exports.NsVueNami = {
+  install: function install(Vue, options) {
+    var vm = new Vue();
+    Vue.prototype.$nami = (0, _nami.nami)(vm);
+  }
+};
+
+exports.default = NsVueNami;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _nami = __webpack_require__(2);
+
+Object.keys(_nami).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _nami[key];
+    }
+  });
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.nami = exports.authGuard = exports.back = exports.modal = exports.navigate = exports.current = exports.register = exports.init = undefined;
+
+var _utils = __webpack_require__(3);
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var routes = [];
+var guard = null;
 var vm = null;
+var currentRoute = '';
 
-var getRoute = function getRoute(name) {
-  var route = routes.find(function (route) {
-    return route.name === name;
+var init = exports.init = function init(options) {
+  routes = options.routes || [];
+  var entry = routes.find(function (route) {
+    return route.entry;
   });
-  return route;
+  if (!entry) {
+    console.warn('No entry component sepecified. Deafaulting to first component in routes array.');
+    entry = routes[0];
+  }
+  return entry.component;
 };
 
-var register = function register(rts) {
-  routes = rts;
+var register = exports.register = function register(route) {
+  routes.push(route);
 };
 
-var navigate = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(name, props) {
-    var _getRoute, component, beforeEnter, route;
+var current = exports.current = function current() {
+  return (0, _utils.getRoute)(currentRoute, routes);
+};
+
+var navigate = exports.navigate = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(routeName, props) {
+    var _getRoute, name, component, meta, noAuth, route;
 
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _getRoute = getRoute(name), component = _getRoute.component, beforeEnter = _getRoute.beforeEnter;
+            _getRoute = (0, _utils.getRoute)(routeName, routes), name = _getRoute.name, component = _getRoute.component, meta = _getRoute.meta, noAuth = _getRoute.noAuth;
             route = null;
 
-            if (!beforeEnter) {
+            if (noAuth) {
               _context.next = 6;
               break;
             }
 
             _context.next = 5;
-            return new Promise(beforeEnter);
+            return new Promise(function (resolve) {
+              return guard(resolve);
+            });
 
           case 5:
             route = _context.sent;
 
           case 6:
 
-            if (route) vm.$nami.navigate(route);else vm.$navigateTo(component, { props: props });
+            if (route) {
+              currentRoute = route.name;
+              vm.$nami.navigate(route);
+            } else {
+              currentRoute = name;
+              vm.$navigateTo(component, { props: props });
+            }
 
           case 7:
-          case "end":
+          case 'end':
             return _context.stop();
         }
       }
@@ -130,30 +198,75 @@ var navigate = function () {
   };
 }();
 
-var back = function back() {
-  vm.$navigateBack();
-};
-
-var modal = function modal(name, props) {
-  var _getRoute2 = getRoute(name),
+var modal = exports.modal = function modal(name, props) {
+  var _getRoute2 = (0, _utils.getRoute)(name),
       component = _getRoute2.component;
 
   vm.$showModal(component, { props: props });
 };
 
-var NsVueNami = exports.NsVueNami = {
-  install: function install(Vue, options) {
-    vm = new Vue();
-    Vue.prototype.$nami = {
-      register: register,
-      navigate: navigate,
-      modal: modal,
-      back: back
-    };
-  }
+var back = exports.back = function back() {
+  vm.$navigateBack();
 };
 
-exports.default = NsVueNami;
+// Hooks
+var authGuard = exports.authGuard = function authGuard(fn) {
+  guard = fn;
+};
+
+var nami = exports.nami = function nami(vi) {
+  vm = vi;
+  return {
+    init: init,
+    register: register,
+    current: current,
+    navigate: navigate,
+    modal: modal,
+    back: back,
+    authGuard: authGuard
+  };
+};
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _utils = __webpack_require__(4);
+
+Object.keys(_utils).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _utils[key];
+    }
+  });
+});
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var getRoute = exports.getRoute = function getRoute(name, routes) {
+  var result = routes.find(function (route) {
+    return route.name === name;
+  });
+  if (!result) return 'not found';
+  return result;
+};
 
 /***/ })
 /******/ ]);
